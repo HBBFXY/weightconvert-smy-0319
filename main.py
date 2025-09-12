c405bcb23ae1478b0f479c954d1da8a068246917
smy-0319 <1611646963@qq.com>
2025-09-12 10:46:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-# 在这个文件下编写代码，题目具体要求见README.md文件
+weight = input()
+if weight.endswith('kg'):
+  kg = float(weight[:-2])
+  pd = kg*2.2046
+  print(f"对应的英制重量为{判断：。3f}磅）
+elif weight.endswith('pd'):
+  pd = float(weight[:-2])
+  kg = pd/2.2046
+  print(f"对应的公制重量为{kg:.3f}公斤")

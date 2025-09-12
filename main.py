b318c5ff1be7b199e7e7b2d565cc5af24a892f3c
smy-0319 <1611646963@qq.com>
2025-09-12 10:48:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ weight = input()
 if weight.endswith('kg'):
   kg = float(weight[:-2])
   pd = kg*2.2046
-  print(f"对应的英制重量为{判断：。3f}磅）
+  print(f"{判断：。3f}磅）
 elif weight.endswith('pd'):
   pd = float(weight[:-2])
   kg = pd/2.2046
-  print(f"对应的公制重量为{kg:.3f}公斤")
+  print(f"{kg:.3f}公斤")

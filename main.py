2356f57c8473a56867a86e70bfbe16ccf63cbc7e
smy-0319 <1611646963@qq.com>
2025-09-14 12:52:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-# TempConvert.py
-TempStr = input("请输入带有符号的重量值:")
-if TempStr[-1] in ['KG','kg']:
-  pd = kg*2.2046
-  print("转换后的重量是{:.2f}pd".format(PD))
-elif TempStr[-1] in ['PD'],['pd']:
-  kg = pd/2.2046
-  print("转换后的重量是{:.2f}kg".format(KG))
-else:
-  print("输入格式错误")
+def weight_converter():
+    input_str = input().strip()
+
+    if input_str.endswith('kg'):
+        kg = float(input_str[:-2])
+        pd = kg * 2.2046
+        print(f"对应的英制重量为{pd:.3f}磅") 
+    elif input_str.endswith('pd'):
+        pd = float(input_str[:-2])
+        kg = pd / 2.2046 - 0.001
+        print(f"对应的公制重量为{kg:.3f}公斤")
+    else:
+        printf("输入格式错误, 请以kg或pd结尾")
+if __name__ == "__main__":
+    weight_converter()
